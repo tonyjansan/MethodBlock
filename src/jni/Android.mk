cd7ed388f99b904f5a2291bfe384c3565d5864b6
Tony Jiang <jiangty2@lenovo.com>
2014-10-16 18:14:04 +0800

    refine Makefile for all platform

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -6,16 +6,7 @@ LOCAL_PATH := $(call my-dir)
 
 LOCAL_ARM_MODE := arm
 LOCAL_SRC_FILES := ../main_arm32.c ../methodblock.c ../lzss.c
-LOCAL_CFLAGS := -Wall -DCROSS_COMPILE
-LOCAL_CFLAGS += -fdefer-pop -fguess-branch-probability -fcprop-registers -floop-optimize\
-                -fif-conversion -fif-conversion2 -ftree-ccp -ftree-dce -ftree-dominator-opts\
-                -ftree-dse -ftree-ter -ftree-lrs -ftree-sra -ftree-copyrename -ftree-fre\
-                -ftree-ch -funit-at-a-time -fmerge-constants -fthread-jumps -fcrossjumping\
-                -foptimize-sibling-calls -fcse-follow-jumps -fcse-skip-blocks -fgcse -fgcse-lm\
-                -fexpensive-optimizations -fstrength-reduce -frerun-cse-after-loop -fpeephole2\
-                -fcaller-saves -fschedule-insns -fschedule-insns2 -fsched-interblock -fsched-spec\
-                -fregmove -fstrict-aliasing -fdelete-null-pointer-checks -freorder-blocks\
-                -falign-functions -falign-jumps -falign-loops -falign-labels -ftree-vrp -ftree-pre
+LOCAL_CFLAGS := -O2 -Wall -DCROSS_COMPILE -fno-reorder-functions
 
 LOCAL_MODULE := methodblock
 LOCAL_MODULE_FILENAME := methodblock
